ff89b31cc5719a763e27c51078e5add682fde83b
David MOLINARI <davvmoli@gmail.com>
2016-12-08 11:28:58 +0100

    redirectToRoute done

--- src/AppBundle/Controller/TutorielCtrlController.php
+++ src/AppBundle/Controller/TutorielCtrlController.php
@@ -13,7 +13,7 @@ class TutorielCtrlController extends Controller
      *
      *     path="/hello/{meh}",
      *     name="TutorielCtrl_hello",
-     *     requirements={"nom" : "[A-Za-z]+"}
+     *     requirements={"meh" : "[A-Za-z]+"}
      * )
      *
      */
@@ -21,8 +21,20 @@ class TutorielCtrlController extends Controller
     {
         $page = "<!DOCTYPE html><html><body><h1>BONJOUR " . $meh . " </h1></body></html>";
         return new Response($page);
-
-
     }
 
+    /**
+     * @Route(
+     *
+     *     path="/redirect/{meh}",
+     *     name="TutorielCtrl_redirect"
+     * )
+     *
+     */
+    public function redirectAction($meh)
+    {
+        $newMeh = $meh . " Apres redirection !";
+        $newMeh = preg_replace("#[^a-zA-Z]#", "", $newMeh);
+        return $this->redirectToRoute('TutorielCtrl_hello', array('meh' => $newMeh));
+    }
 }
