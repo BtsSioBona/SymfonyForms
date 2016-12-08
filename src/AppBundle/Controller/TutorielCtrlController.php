12755b6dac4c1a765a75e195e0646516217e1010
David MOLINARI <davvmoli@gmail.com>
2016-12-08 11:36:41 +0100

    Add transfertAction => forward

--- src/AppBundle/Controller/TutorielCtrlController.php
+++ src/AppBundle/Controller/TutorielCtrlController.php
@@ -11,30 +11,46 @@ class TutorielCtrlController extends Controller
     /**
      * @Route(
      *
-     *     path="/hello/{meh}",
+     *     path="/hello/{nom}",
      *     name="TutorielCtrl_hello",
-     *     requirements={"meh" : "[A-Za-z]+"}
+     *     requirements={"nom" : "[A-Za-z]+"}
      * )
      *
      */
-    public function helloAction($meh)
+    public function helloAction($nom)
     {
-        $page = "<!DOCTYPE html><html><body><h1>BONJOUR " . $meh . " </h1></body></html>";
+        $page = "<!DOCTYPE html><html><body><h1>BONJOUR " . $nom . " </h1></body></html>";
         return new Response($page);
     }
 
     /**
      * @Route(
      *
-     *     path="/redirect/{meh}",
+     *     path="/redirect/{nom}",
      *     name="TutorielCtrl_redirect"
      * )
      *
      */
-    public function redirectAction($meh)
+    public function redirectAction($nom)
     {
-        $newMeh = $meh . " Apres redirection !";
+        $newMeh = $nom . " Apres redirection !";
         $newMeh = preg_replace("#[^a-zA-Z]#", "", $newMeh);
-        return $this->redirectToRoute('TutorielCtrl_hello', array('meh' => $newMeh));
+        return $this->redirectToRoute('TutorielCtrl_hello', array('nom' => $newMeh));
+    }
+
+
+    /**
+     * @param $nom
+     * @return Response
+     *
+     * @Route(
+     *     path="/transferer/{nom}",
+     *     name="TutorielCtrl_transferer",
+     *     requirements={"nom" : "[A-Za-z]+"}
+     * )
+     */
+    public function transfertAction($nom){
+        $param = $nom . " (Transféré  à " . date("d/m/Y - H:i") . " ) ";
+        return $this->forward('AppBundle:TutorielCtrl:hello', array('nom' => $param));
     }
 }
